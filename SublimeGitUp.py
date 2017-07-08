923c95e11f01946e3e7ffe02022f55cf9cd589a3
Koen Lageveen <koen.lageveen@me.com>
2017-07-08 15:07:50 +0200

    add sidebar action

--- SublimeGitUp.py
+++ SublimeGitUp.py
@@ -26,3 +26,26 @@ class GitupOpenCommand(sublime_plugin.WindowCommand):
 
         app_path = '/Applications/GitUp.app'
         subprocess.call(['open', '-a', app_path, path])
+
+class SideBarGitupCommand(sublime_plugin.WindowCommand):
+
+    def is_enabled(self):
+        return True
+
+    def get_path(self, paths):
+        try:
+            return paths[0]
+        except IndexError:
+            return self.window.active_view().file_name()
+
+    def run(self, paths):
+        sublime.status_message('GitUp: running')
+        path = self.get_path(paths)
+        if not path:
+            sublime.status_message('GitUp: No path')
+            return False
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+
+        app_path = '/Applications/GitUp.app'
+        subprocess.call(['open', '-a', app_path, path])
